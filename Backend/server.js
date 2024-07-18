b077a3ccf8f4e5f6658815f7b7ddb480f1cca1f9
MagedNour <magednour1995@gmail.com>
2024-07-18 04:07:30 +0300

    first commit

---
+++ Backend/server.js
@@ -0,0 +1,64 @@
+import express from 'express';
+import cors from 'cors'
+
+
+const app = express();
+const port = 5500;
+
+app.use(express.static('public'));
+app.use(cors());
+
+app.get('/data', (req, res,next) => {
+  const data = {
+    "customers": [
+      { "id": 1, "name": "Ahmed Ali" },
+      { "id": 2, "name": "Aya Elsayed" },
+      { "id": 3, "name": "Mina Adel" },
+      { "id": 4, "name": "Sarah Reda" },
+      { "id": 5, "name": "Mohamed Sayed" },
+      { "id": 6, "name": "Maged Nour" },
+      { "id": 7, "name": "Nora Kamal" },
+      { "id": 8, "name": "Hassan Mostafa" },
+      { "id": 9, "name": "Laila Hassan" },
+      { "id": 10, "name": "Youssef Amin" }
+    ],
+    "transactions": [
+      { "id": 1, "customer_id": 1, "date": "2022-01-01", "amount": 1000 },
+      { "id": 2, "customer_id": 1, "date": "2022-01-02", "amount": 2000 },
+      { "id": 3, "customer_id": 2, "date": "2022-01-01", "amount": 550 },
+      { "id": 4, "customer_id": 3, "date": "2022-01-01", "amount": 500 },
+      { "id": 5, "customer_id": 2, "date": "2022-01-02", "amount": 1300 },
+      { "id": 6, "customer_id": 4, "date": "2022-01-01", "amount": 750 },
+      { "id": 7, "customer_id": 3, "date": "2022-01-02", "amount": 1250 },
+      { "id": 8, "customer_id": 5, "date": "2022-01-01", "amount": 2500 },
+      { "id": 9, "customer_id": 5, "date": "2022-01-02", "amount": 875 },
+      { "id": 10, "customer_id": 1, "date": "2022-01-10", "amount": 700 },
+      { "id": 11, "customer_id": 2, "date": "2022-01-11", "amount": 1010 },
+      { "id": 12, "customer_id": 3, "date": "2022-01-12", "amount": 30 },
+      { "id": 13, "customer_id": 5, "date": "2022-01-11", "amount": 2400 },
+      { "id": 14, "customer_id": 4, "date": "2022-01-10", "amount": 1700 },
+      { "id": 15, "customer_id": 4, "date": "2022-01-13", "amount": 3400 },
+      { "id": 16, "customer_id": 1, "date": "2022-01-14", "amount": 200 },
+      { "id": 17, "customer_id": 1, "date": "2022-01-15", "amount": 3000 },
+      { "id": 18, "customer_id": 6, "date": "2022-02-01", "amount": 1000 },
+      { "id": 19, "customer_id": 6, "date": "2022-02-05", "amount": 300 },
+      { "id": 20, "customer_id": 6, "date": "2022-02-10", "amount": 1200 },
+      { "id": 21, "customer_id": 7, "date": "2022-02-11", "amount": 800 },
+      { "id": 22, "customer_id": 7, "date": "2022-02-12", "amount": 1500 },
+      { "id": 23, "customer_id": 8, "date": "2022-02-13", "amount": 2000 },
+      { "id": 24, "customer_id": 8, "date": "2022-02-14", "amount": 950 },
+      { "id": 25, "customer_id": 9, "date": "2022-02-15", "amount": 1300 },
+      { "id": 26, "customer_id": 9, "date": "2022-02-16", "amount": 400 },
+      { "id": 27, "customer_id": 10, "date": "2022-02-17", "amount": 1800 },
+      { "id": 28, "customer_id": 10, "date": "2022-02-18", "amount": 1600 },
+      { "id": 29, "customer_id": 10, "date": "2022-02-19", "amount": 1200 },
+      { "id": 30, "customer_id": 9, "date": "2022-02-20", "amount": 750 }
+    ]
+  };
+
+  res.json(data);
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
---
+++ Frontend/src/JavaScript/index.js
@@ -0,0 +1,316 @@
+let chartInstance = null;
+let nameSorted = false;
+
+
+
+// Init HomePage
+$(document).ready(async function () {
+
+  const { transactionsArray, customersArray } = await getData();
+
+  // Filter by Customer Name
+  $('#customer-filter').on('input', function () {
+    const filter = this.value.toLowerCase();
+    const customerIds = customersArray.filter(c => c.name.toLowerCase().includes(filter)).map(c => c.id);
+
+    const filteredTransactions = transactionsArray.filter(transaction => customerIds.includes(transaction.customer_id));
+
+    displayTransactions(filteredTransactions)
+  });
+
+  // Filter by Transaction Amount
+  $("#amount-filter").on('input', function () {
+    let filter = parseFloat(this.value);
+    filter = isNaN(filter) ? 0 : filter;
+    const filteredTransactions = transactionsArray.filter(t => filter <= t.amount)
+    displayTransactions(filteredTransactions)
+  })
+
+  // open Customer's page
+  $(document).on('click', '.transactions-row', function () {
+
+
+
+    let customer = $(this).find('td').first().text();
+    customer = customersArray.find(c => c.name == customer);
+
+    let customerTransactions = transactionsArray.filter(t => t.customer_id === customer.id);
+    let totalTransactions = 0;
+    customerTransactions.forEach(t => totalTransactions += t.amount)
+    customer.transaction = customerTransactions;
+    customer.totalTransactions = totalTransactions;
+
+
+    $("#viewport").empty();
+    displayCustomerPage(customer)
+
+  })
+
+  // Return or Reload HomePage
+  $(".logo").on('click', () => getData())
+
+  //SortByName ==> adding click
+  $("#sortByName").on('click', function () {
+    if(nameSorted){
+      displayTransactions(transactionsArray)
+      nameSorted = false
+      $("#sortByName").css({"color": "gray"})
+    }else{
+      sortByName([...transactionsArray]);
+      $("#sortByName").css({"color": "black"})
+    }
+    
+  })
+})
+
+function displayHomePage(transactionsArray, customersArray) {
+  $("#viewport").empty();
+
+  let customersCount = customersArray.length;
+  let transactionsCount = transactionsArray.length;
+  let totalTransactionsAmount = 0;
+  transactionsArray.forEach(t => totalTransactionsAmount += t.amount);
+
+  $('#viewport').append(`
+  <header>
+  
+ <div class="flex flex-wrap">
+  <div class="p-5 w-full md:w-1/3">
+      <div class="bg-white  p-10 rounded-lg shadow-lg mb-6 min-h-full">
+          <h1 class="text-2xl font-bold mb-2 flex justify-around">
+              <i class="fa-solid fa-users fa-3x"></i>
+              <span class="text-3xl mt-5">${customersCount}</span>
+          </h1>
+          <p class="text-center mt-10 font-semibold text-2xl">Customers</p>
+      </div>
+  </div>
+  <div class="p-5 w-full md:w-1/3">
+      <div class="bg-white p-10 rounded-lg shadow-lg mb-6 min-h-full">
+          <h1 class="text-2xl font-bold mb-2 flex justify-around">
+          <i class="fa-solid fa-hand-holding-dollar fa-3x"></i>
+          <span class="text-3xl mt-5">${transactionsCount}</span></h1>
+          <p class="text-2xl text-center mt-10"><span class="font-semibold">Total: </span>${totalTransactionsAmount} E£</p>
+      </div>
+  </div>
+  <div class="p-5 w-full md:w-1/3">
+      <div class="bg-white p-2 rounded-lg shadow-lg mb-6 min-h-full">
+          <h2 class="text-2xl font-bold mb-2 text-center">Transactions Graph</h2>
+          <canvas id="totalTransactionChart"></canvas>
+      </div>
+  </div>
+ </div>
+ 
+</header>
+
+
+<h1 class="my-4 text-2xl font-semibold">Customer Transactions</h1>
+<div class="flex space-x-4 mb-4">
+  <input type="text" id="customer-filter" class="form-control filter-input w-1/2 px-3 py-2 border rounded-md"
+      placeholder="Filter by customer name">
+  <input type="number" id="amount-filter" class="form-control filter-input w-1/2 px-3 py-2 border rounded-md"
+      placeholder="Filter by transaction amount">
+</div>
+
+<table class="w-full mt-10 border-b-2 border-gray-200">
+  <thead>
+      <tr class="border-b-2">
+          <th class="px-4 py-4">Customer Name
+          <i class="fa-solid fa-sort ms-2 cursor-pointer transition-colors duration-200 text-gray-400" id ="sortByName"></i>
+          </th>
+          <th class="px-4 py-4">Transaction Date</th>
+          <th class="px-4 py-4">Transaction Amount</th>
+      </tr>
+  </thead>
+  <tbody id="transaction-table-body">
+      <!-- Data will be inserted here by JavaScript -->
+  </tbody>
+
+</table>
+  `)
+
+  displayTransactions(transactionsArray);
+}
+
+async function getData() {
+  try {
+    let api = await fetch('http://localhost:5500/data', { method: 'GET' });
+    let response = await api.json();
+    let customersArray = response.customers;
+    let transactionsArray = response.transactions;
+
+    transactionsArray.forEach(trans => {
+      trans.name = customersArray.find(c => c.id == trans.customer_id).name;
+    })
+
+    displayHomePage(transactionsArray, customersArray)
+
+    return ({ transactionsArray, customersArray });
+
+
+  } catch (error) {
+    console.error('Error fetching data:', error);
+  }
+}
+
+function displayTransactions(trans) {
+  $('#transaction-table-body').empty();
+
+  let allRows = ``
+
+  trans.forEach(trans => {
+
+    allRows += `
+          <tr class = "transactions-row cursor-pointer text-center border-b-2 transition-all duration-200 hover:bg-gray-200 hover:font-semibold" >
+            <td class= "px-4 py-4 text-blue-500 duration-300 ">${trans.name}</td>
+            <td class= "px-4 py-4">${trans.date}</td>
+            <td class= " px-4 py-4">${trans.amount}</td>
+          </tr>
+    `
+
+  });
+
+  $('#transaction-table-body').append(allRows)
+  let dataObject = {}
+  dataObject.transaction = trans;
+  dataObject.home = true;
+
+  createChart(dataObject)
+
+
+}
+
+
+function sortByName(dataArray) {
+
+  dataArray.sort((a, b) => {
+    if (a.name < b.name) {
+      return -1;
+    } else if (a.name > b.name) {
+      return 1;
+    } else {
+      return 0;
+    }
+  })
+
+nameSorted = true;
+
+  displayTransactions(dataArray)
+
+}
+
+
+
+// Customer Page
+function displayCustomerPage(customer) {
+  let transactionRows = ``
+
+
+  for (let i = 0; i < customer.transaction.length; i++) {
+    transactionRows += `
+    <tr>
+        <td class="py-2 px-4 border-b text-center">${customer.transaction[i].date}</td>
+        <td class="py-2 px-4 border-b text-center">${customer.transaction[i].amount}</td>
+    </tr>
+    `
+  }
+
+  $("#viewport").append(`
+  <div class="p-6 rounded-lg shadow-lg mb-6 flex flex-wrap">
+
+  <div class= "w-full md:w-2/4 mb-5">
+  <h1 class="text-2xl font-bold mb-2 me-10">
+  <i class="fa-solid fa-user fa-lg text-blue-300 me-3"></i>
+  <span>${customer.name}</span>
+  </h1>
+  </div>
+ 
+
+  <div class= "w-full md:w-2/4 mb-5">
+  <h1 class="text-2xl font-bold mb-2">
+  <i class="fa-solid fa-sack-dollar text-green-200 fa-lg me-3"></i>
+  Total Amount: 
+  <span class="font-light">${customer.totalTransactions} E£</span>
+  </h1>
+  </div>
+  
+</div>
+
+<!-- Transactions Table Section -->
+<div class="bg-white p-6 rounded-lg shadow-lg mb-6">
+  <h2 class="text-xl font-bold mb-4">Transactions</h2>
+  <table class="w-2/3 m-auto bg-white">
+      <thead>
+          <tr>
+              <th class="py-2 px-4 border-b">Date</th>
+              <th class="py-2 px-4 border-b">Amount</th>
+          </tr>
+      </thead>
+      <tbody id="transactionsTableBody">
+          ${transactionRows}
+      </tbody>
+  </table>
+
+  <p class="text-xl mt-4"> <span class="font-semibold">Total Transactions Amount: </span> <span class=""> ${customer.totalTransactions} E£
+  </span></p>
+
+
+</div>
+
+<!-- Transactions Graph Section -->
+<div class="bg-white p-6 rounded-lg shadow-lg">
+  <h2 class="text-xl font-bold mb-4">Transactions Graph</h2>
+  <canvas id="transactionChart" class="w-2/4 h-60 mx-auto"></canvas>
+</div>
+</div>
+  `)
+  createChart(customer)
+}
+
+
+// Create transactions graph
+function createChart(dataObject) {
+
+  if (chartInstance != null) {
+    chartInstance.destroy();
+  }
+
+  //check if the graph is at home or customer's page
+  if (dataObject.home) {
+    var ctx = document.getElementById('totalTransactionChart').getContext('2d');
+    chartType = 'bar'
+  } else {
+    var ctx = document.getElementById('transactionChart').getContext('2d');
+    chartType = 'line'
+  }
+
+
+  chartInstance = new Chart(ctx, {
+    type: chartType,
+    data: {
+      labels: dataObject.transaction.map(transaction => transaction.date),
+      datasets: [{
+        label: 'Transaction Amount',
+        data: dataObject.transaction.map(transaction => transaction.amount),
+        borderColor: 'rgba(75, 192, 192, 1)',
+        backgroundColor: '#BFDBFE',
+        borderWidth: 1,
+        fill: false,
+        tension: 0.1
+      }]
+    },
+    options: {
+      scales: {
+        x: {
+          type: 'time',
+          time: {
+            unit: 'day'
+          }
+        }
+      }
+
+    }
+  });
+}
+
+
+
